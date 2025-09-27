c846594511706a1baa5195c834b05183badf242a
A-Sadaawi <ahmdsdawy88@gmail.com>
2025-09-27 14:44:54 +0300

    Add the insert method to AVL tree explain how rotations works with insert

--- Tree/Binary tree/AVL Tree/AVL.cpp
+++ Tree/Binary tree/AVL Tree/AVL.cpp
@@ -20,19 +20,21 @@ class AVL {
      int getBalance(Node* node);
      Node* rotation_R (Node* node);
      Node* rotation_L (Node* node);
+     Node* findsuitPlace(Node* node, int key);
+     void insert(Node* node, int key);
      void inorder(Node* node);
 
 
     public:
      AVL () : root(nullptr){}
      int getHi() {return getHi(root);}
+     void  insert(int key) {return insert(root, key);}
      void inorder() {inorder(root);}
 };
 
 int AVL::getHi(Node* node){
-    if(node == nullptr)return ;
+    if(node == nullptr)return 0 ;
     return node->hi;
-    return 0;
 }
 void AVL ::modifyHi(Node* node){
     node->hi = 1 + max(node->left ? node->left->hi : 0, node->right ? node->right->hi : 0);
@@ -61,10 +63,12 @@ Node* AVL::rotation_R(Node* node){
      modifyHi(temp);
 return temp;
 }
+
 int AVL::getBalance(Node *node)
 {
     return node ? getHi(node->left) - getHi(node->right) : 0;  
 }
+
 // Rotation Right to fix LL problem
 Node* AVL::rotation_L(Node* node){
 
@@ -83,6 +87,60 @@ Node* AVL::rotation_L(Node* node){
      modifyHi(node);
      modifyHi(temp);
 return temp;
+}
+
+Node* AVL ::findsuitPlace(Node* node, int key){
+  if(node == nullptr){
+    Node* newNode = new Node(key);
+    return newNode;
+  }
+  if(key < node->val)
+  {
+    node->left = findsuitPlace(node->left, key);
+    node->left->parent = node;
+  }
+  if(key > node->val){
+    node->right = findsuitPlace(node->right, key);
+    node->right->parent = node;
+  }
+  return node;
+}
+
+void AVL ::insert(Node* node, int key){
+  Node* suitP = findsuitPlace(node, key);
+  while(suitP->parent != nullptr){
+    // with evey iteration update the hight
+    modifyHi(suitP);
+    int b = getBalance(suitP);
+    // How the rotation algorithm work?
+      // - we check first the balance region [-1,0,1]
+      // - then check do we need double or single rotatoin
+    if(b > 1){
+      if(getBalance(suitP->left) >= 0){
+        // LL Problem
+        suitP = rotation_R(suitP);
+      }else {
+        // LR problem
+        suitP->left = rotation_L(suitP->left);
+        suitP = rotation_R(suitP);
+      }
+      if(suitP->parent == nullptr) root = suitP;
+    } 
+    if(b < -1)
+    {
+      if(getBalance(suitP->right) <= -1){
+        //RR Problem
+        suitP = rotation_L(suitP);
+      }else{
+        //RL problem
+        suitP->right = rotation_R(suitP->right);
+        suitP = rotation_L(suitP);
+      }
+      if(suitP->parent == nullptr) root = suitP;
+    } 
+    suitP = suitP->parent;
+  }
+   
 }
 
 void AVL:: inorder(Node* node){
